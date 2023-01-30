79959aba2f8dea2400ccb67a5c2175de7c1f36ad
kawasemi2k1 <bovainghiengnang2k1@gmail.com>
2023-01-30 12:14:41 +0700

    Add name and age to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_10_043206) do
+ActiveRecord::Schema.define(version: 2023_01_10_102004) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
